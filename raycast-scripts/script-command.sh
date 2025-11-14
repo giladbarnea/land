500516b9db9cdeb815562336de59ddbad31d0bf4
Gilad Barnea <gilad@hear.ai>
2025-11-14 14:32:13 +0200

    add raycast-scripts/

---
+++ raycast-scripts/script-command.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Raycast Script Command Template
+#
+# Duplicate this file and remove ".template." from the filename to get started.
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title kitty quick-access shift-shift
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Raycast Scripts
+
+/Applications/kitty.app/Contents/MacOS/kitten quick-access-terminal
