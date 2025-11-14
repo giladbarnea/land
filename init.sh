42fc5060672479771d25630df0690e7441814758
Gilad Barnea <gilad@hear.ai>
2025-11-14 14:38:16 +0200

    init.sh: source scraping.sh and convert.sh
    Add scraping.sh and convert.sh to initialization sequence

--- init.sh
+++ init.sh
@@ -17,33 +17,35 @@ THIS_SCRIPT_DIR="${0%/*}"
 # [[ -e "/usr/share/powerline/bindings/zsh/powerline.zsh" && -z "$BATS_TEST_FILENAME" ]] && source "/usr/share/powerline/bindings/zsh/powerline.zsh"
 
 source "$THIS_SCRIPT_DIR/environment.sh"
-source "$THIS_SCRIPT_DIR/aliases.sh"      # docker.sh relies on alias cut=gcut
+source "$THIS_SCRIPT_DIR/aliases.sh" # docker.sh relies on alias cut=gcut
 source "$THIS_SCRIPT_DIR/term.zsh"
 source "$THIS_SCRIPT_DIR/log.sh"
 source "$THIS_SCRIPT_DIR/util.sh"
 source "$THIS_SCRIPT_DIR/inspect.sh"
 source "$THIS_SCRIPT_DIR/python.sh"
-source "$THIS_SCRIPT_DIR/str.sh"    	  # Maybe before python.sh?
+source "$THIS_SCRIPT_DIR/str.sh" # Maybe before python.sh?
 source "$THIS_SCRIPT_DIR/tools.sh"
 source "$THIS_SCRIPT_DIR/pretty.sh"
 source "$THIS_SCRIPT_DIR/llm.zsh"
-source "$THIS_SCRIPT_DIR/nav.sh"          # 'cd' related functions
-source "$THIS_SCRIPT_DIR/fs.sh"   		  # mkcdir, mountdevice
+source "$THIS_SCRIPT_DIR/nav.sh" # 'cd' related functions
+source "$THIS_SCRIPT_DIR/fs.sh"  # mkcdir, mountdevice
 source "$THIS_SCRIPT_DIR/notify.sh"
 source "$THIS_SCRIPT_DIR/git.sh"
 source "$THIS_SCRIPT_DIR/ghcli.sh"
-source "$THIS_SCRIPT_DIR/history.sh"  	  # hs/hsi/hl
+source "$THIS_SCRIPT_DIR/history.sh" # hs/hsi/hl
 source "$THIS_SCRIPT_DIR/async.zsh"
-source "$THIS_SCRIPT_DIR/pkgmgr.sh"       # brew, apt, snap
-source "$THIS_SCRIPT_DIR/misc.sh"		  # editfile, countlines, bashh, zshh
+source "$THIS_SCRIPT_DIR/pkgmgr.sh" # brew, apt, snap
+source "$THIS_SCRIPT_DIR/misc.sh"   # editfile, countlines, bashh, zshh
 source "$THIS_SCRIPT_DIR/fzf.sh"
-source "$THIS_SCRIPT_DIR/paths.sh"       # cppwd, cppath, resolve
+source "$THIS_SCRIPT_DIR/paths.sh" # cppwd, cppath, resolve
 source "$THIS_SCRIPT_DIR/keybinds.zsh"
+source "$THIS_SCRIPT_DIR/scraping.sh"
+source "$THIS_SCRIPT_DIR/convert.sh"
 # [[ "$ZSH" ]] && source "$THIS_SCRIPT_DIR/zsh_advanced_history.zsh"
 
 # shellcheck disable=SC2188
 if is_interactive; then
-    source <(<"$THIS_SCRIPT_DIR"/completions/_*)
+  source <(<"$THIS_SCRIPT_DIR"/completions/_*)
 fi
 
 # shellcheck disable=SC2188
