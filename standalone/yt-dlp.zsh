5c43a0708271226741e51df6f96a8b7482a90c78
Gilad Barnea <gilad@hear.ai>
2025-11-14 14:39:19 +0200

    standalone/yt-dlp.zsh: add idempotency, improve model usage, add delays
    - Make subtitle download idempotent (check for existing files)
    - Switch to 5m model for better quality
    - Add sleep delays between parallel jobs
    - Update script path references to use $LAND

--- standalone/yt-dlp.zsh
+++ standalone/yt-dlp.zsh
@@ -77,74 +77,81 @@ function yt-dlp.subs(){
 	[[ "$url" ]] || { log.error "$0: Not enough args (expected 1, got ${#$}). Usage:\n$(docstring "$0")"; return 2; }
 	
 	
-	# --- Get title and available subtitles
+	# --- Get title
 	local title="$(yt-dlp "$url" --ignore-config --get-title)" || { log.error "Failed getting title"; return 1; }
 	local title_path_appropriate="$(topathlike "$title" -f)"
 	mkdir -p "./${title_path_appropriate}" || { log.error "Failed creating directory: ./${title_path_appropriate}"; return 1; }
-	local subtitle_list_raw="$(yt-dlp "$url" --ignore-config --no-mark-watched --list-subs)"
 	
-	command grep -q 'has no subtitles' <<< "$subtitle_list_raw" && {
-		log.warn "Listing subtitles also outputted that there are no subtitles available. Continuing anyway.";
-	}
 	
-	.dl-subs(){
-		vex ---just-run yt-dlp --restrict-filenames --no-mark-watched --skip-download "$@" 1>&2
-	}
-	local subtitle_lang
-	local any_subtitles_downloaded_succesfully=false
-	local -a available_eng_subtitles=( $(echo "$subtitle_list_raw" | awk '/^en/{print $1}') )
+	# --- Get available subtitles idempotently
+	any_subtitles_downloaded_succesfully=false
+	[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] \
+		&& any_subtitles_downloaded_succesfully=true
 	
-	if [[ ${available_eng_subtitles} ]]; then
-		log.info "English subtitles available: ${available_eng_subtitles[*]}"
-		if [[ ${available_eng_subtitles[(r)en-orig]} ]]; then
-			subtitle_lang=en-orig
-		elif [[ ${available_eng_subtitles[(r)en-US]} ]]; then
-			subtitle_lang=en-US
-		elif [[ ${available_eng_subtitles[(r)en]} ]]; then
-			subtitle_lang=en
-		else
-			subtitle_lang="$(input "Sub languages 'en-orig', 'en-US' and 'en' are unavailable. Choose subtitle language:" --choices="( "${available_eng_subtitles[*]}" )")"
-		fi
-		log.info "Title: ${title}. Downloading proper subtitles into ./${title_path_appropriate}/subtitles..."
+	if [[ "$any_subtitles_downloaded_succesfully" = false ]]; then
+		local subtitle_list_raw="$(yt-dlp "$url" --ignore-config --no-mark-watched --list-subs)"
 		
-		# --- Download real subtitles
-		.dl-subs "$url" --write-subs --sub-langs="${subtitle_lang}" --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
+		command grep -q 'has no subtitles' <<< "$subtitle_list_raw" && {
+			log.warn "Listing subtitles also outputted that there are no subtitles available. Continuing anyway.";
+		}
+		
+		.dl-subs(){
+			vex ---just-run yt-dlp --restrict-filenames --no-mark-watched --skip-download "$@" 1>&2
+		}
+		local subtitle_lang
+		local any_subtitles_downloaded_succesfully=false
+		local -a available_eng_subtitles=( $(echo "$subtitle_list_raw" | awk '/^en/{print $1}') )
+		
+		if [[ ${available_eng_subtitles} ]]; then
+			log.info "English subtitles available: ${available_eng_subtitles[*]}"
+			if [[ ${available_eng_subtitles[(r)en-orig]} ]]; then
+				subtitle_lang=en-orig
+			elif [[ ${available_eng_subtitles[(r)en-US]} ]]; then
+				subtitle_lang=en-US
+			elif [[ ${available_eng_subtitles[(r)en]} ]]; then
+				subtitle_lang=en
+			else
+				subtitle_lang="$(input "Sub languages 'en-orig', 'en-US' and 'en' are unavailable. Choose subtitle language:" --choices="( "${available_eng_subtitles[*]}" )")"
+			fi
+			log.info "Title: ${title}. Downloading proper subtitles into ./${title_path_appropriate}/subtitles..."
+			
+			# --- Download real subtitles
+			.dl-subs "$url" --write-subs --sub-langs="${subtitle_lang}" --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
 
+			[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] \
+				&& any_subtitles_downloaded_succesfully=true
+		else
+			log.warn "No english subtitles available. Counting on auto-generated.";
+		fi
+		
+		if [[ "$any_subtitles_downloaded_succesfully" = false ]]; then
+			# --- Download auto generated subtitles
+			log.warn "No real subtitles downloaded. Attempting to download auto-generated subtitles..."
+			.dl-subs "$url" --write-auto-subs --sub-langs="${subtitle_lang}" --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
+		fi
+		
+		any_subtitles_downloaded_succesfully=false
 		[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] \
 			&& any_subtitles_downloaded_succesfully=true
-	else
-		log.warn "No english subtitles available. Counting on auto-generated.";
+		
+		if [[ "$any_subtitles_downloaded_succesfully" = false ]]; then
+			# --- Download real subtitles without specifying language
+			log.warn "No auto-generated subtitles downloaded. Attempting to download real subtitles without specifying language..."
+			.dl-subs "$url" --write-subs --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
+		fi
+		
+		any_subtitles_downloaded_succesfully=false
+		[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] \
+			&& any_subtitles_downloaded_succesfully=true
+		
+		if [[ "$any_subtitles_downloaded_succesfully" = false ]]; then
+		# --- Download auto generated subtitles without specifying language
+			log.warn "No real subtitles downloaded even without specifying language. Attempting to download auto-generated subtitles without specifying language..."
+			.dl-subs "$url" --write-auto-subs --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
+		fi
+		
+		[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] || { log.error "Failed to download subtitles"; return 1; }
 	fi
-	
-	if [[ "$any_subtitles_downloaded_succesfully" = false ]]; then
-	    # --- Download auto generated subtitles
-		log.warn "No real subtitles downloaded. Attempting to download auto-generated subtitles..."
-		.dl-subs "$url" --write-auto-subs --sub-langs="${subtitle_lang}" --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
-	fi
-	
-	
-	
-	any_subtitles_downloaded_succesfully=false
-	[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] \
-		&& any_subtitles_downloaded_succesfully=true
-	
-	if [[ "$any_subtitles_downloaded_succesfully" = false ]]; then
-		# --- Download real subtitles without specifying language
-		log.warn "No auto-generated subtitles downloaded. Attempting to download real subtitles without specifying language..."
-		.dl-subs "$url" --write-subs --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
-	fi
-	
-	any_subtitles_downloaded_succesfully=false
-	[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] \
-		&& any_subtitles_downloaded_succesfully=true
-	
-	if [[ "$any_subtitles_downloaded_succesfully" = false ]]; then
-	# --- Download auto generated subtitles without specifying language
-		log.warn "No real subtitles downloaded even without specifying language. Attempting to download auto-generated subtitles without specifying language..."
-		.dl-subs "$url" --write-auto-subs --sub-format=srv1 --output "./${title_path_appropriate}/subtitles"
-	fi
-	
-	[[ -n ./${title_path_appropriate}/subtitles*.srv1(#qN) ]] || { log.error "Failed to download subtitles"; return 1; }
 	
 	log.success "Downloaded subtitles to ./${title_path_appropriate}/subtitles. Processing subtitles into Markdown..."
 
@@ -153,18 +160,24 @@ function yt-dlp.subs(){
 	yt-dlp.info "$url" > "./${title_path_appropriate}/short-info.json"
 	
 	# Create chapters markdown files
-	python3 "$SCRIPTS"/standalone/yt_dlp_xml_to_md.py ./${title_path_appropriate}/subtitles*.srv1([1]) "./${title_path_appropriate}/short-info.json" --split -o "./${title_path_appropriate}/chapters"
+	python3 "${LAND}"/standalone/yt_dlp_xml_to_md.py ./${title_path_appropriate}/subtitles*.srv1([1]) "./${title_path_appropriate}/short-info.json" --split -o "./${title_path_appropriate}/chapters"
 	
 	local file	
 	# Punctuation
-	log.title "Punctuating and summarizing chapters..."
+	log.title "Created markdown files for chapters. Punctuating and summarizing chapters..."
 	for file in ./${title_path_appropriate}/chapters/*.md; do
 		( 
-			cat "$file" | llm "The attached markdown file, ${file##*/}, contains a chapter of a YouTube video. Punctuate the text so that it makes sense, then join the lines and break them according to the text's meaning. Keep the original text unchanged except for formatting. Think as much as you need, but your final response should simply be the punctuated text, nothing else." --stdin-tag "chapter '${${file##*/}%.*}' of a YouTube video" --no-md -m flash > "${file%.*}.punctuated.md" ;
+			log.title "${file##*/}: Punctuating... (1/2)" -x;
+			cat "$file" | llm "The attached markdown file, ${file##*/}, contains a chapter of a YouTube video. Punctuate the text so that it makes sense, then join the lines and break them according to the text's meaning. Keep the original text unchanged except for formatting. Think as much as you need, but your final response should simply be the punctuated text, nothing else." --stdin-tag "chapter '${${file##*/}%.*}' of a YouTube video" -q --no-clear --no-md -m 5m > "${file%.*}.punctuated.md" ;
 			mv "${file%.*}.punctuated.md" "$file" ;
-			cat "${file}" | llm "The attached markdown file, ${file##*/}, contains a chapter of a YouTube video. Summarize it succinctly and directly, without fluff, but DENSELY. By densely I mean: high signal-to-noise ratio / low entropy / and exhaustive coverage but far from verbose. The summary should be pleasant to read, have a #title, separated to sections if needed for readability, nicely formatted, have bullets if needed, high-quality, captures the high-value takeaways of the chapter, but not verbose. Think as much as you need, but your final response should simply be your summary. If you need to, refer to the text as 'this chapter'." --stdin-tag "chapter '${${file##*/}%.*}' of a YouTube video" --no-md -m flash > "${file%.*}.summary.md" ;
+			log.title "${file##*/}: Summarizing... (2/2)" -x;
+			cat "${file}" | llm "The attached markdown file, ${file##*/}, contains a chapter of a YouTube video. Summarize it succinctly and directly, without fluff, but DENSELY. By densely I mean: high signal-to-noise ratio / low entropy / and exhaustive coverage but far from verbose. The summary should be pleasant to read, have a #title, separated to sections if needed for readability, nicely formatted, have bullets if needed, high-quality, captures the high-value takeaways of the chapter, but not verbose. Think as much as you need, but your final response should simply be your summary. If you need to, refer to the text as 'this chapter'." --stdin-tag "chapter '${${file##*/}%.*}' of a YouTube video" -q --no-clear --no-md -m 5m > "${file%.*}.summary.md" ;
 		) &
+		sleep 1
 	done
+	log.title "Waiting for chapters to be punctuated and summarized..." -x;
+	wait
+	log.title "Done." -x;
 }
 
 
